d17f24eecc5eb56912de04c4bb95bc3ef4882928
Megan St. Hilaire <mfsthilaire13@gmail.com>
2019-06-02 14:49:46 -0400

    More JS Attempts...

--- locationResults.js
+++ locationResults.js
@@ -3,32 +3,30 @@
 	locationResults.js -- This file's code implements display of park/visitor center/campground locations!
 */
 
-const API_KEY = "k8aznKvAdbVkBiEUBIuYA7h4Aq1z8154urrJZLD8"
+const API_KEY = "k8aznKvAdbVkBiEUBIuYA7h4Aq1z8154urrJZLD8";
 
-function dispLocRes() {
+const locURL = "https://developer.nps.gov/api/v1/parks?api_key=" + API_KEY ;
 
-	$.ajax({
-		url: "https://developer.nps.gov/api/v1/parks",
-		dataType: "json",
-		headers: {"Authorization": API_KEY},
-		success: function(data) {
+var apiReq = new XMLHTTPRequest();
 
-			// div displaying results
-			var parkDisplay = $("#locationResults");
+apiReq.open('GET', locURL, true);
+apiReq.onload = function() {
+	// div displaying results
+	var parkDisplay = document.getElementById('locationResults');
 
-			var parkList = data.data;
+	var parkList = JSON.parse(this.response);
 
-			for(pIndex = 0; pIndex < 50; pIndex++) {
-				park = parkList[pIndex];
+	for(pIndex = 0; pIndex < 50; pIndex++) {
+		park = parkList[pIndex];
 
-				var pContent = "<h3>" + park.fullName + "</h3>";
-				pContent = pContent + "<p>" + park.states + "</p>";
+		const pTitle = document.createElement('h3');
+		pTitle.textContent = park.fullName;
 
-				parkDisplay.append(pContent);
+		const pStates = document.createElement('p');
+		pStates.textContent = park.states;
 
-			}
-
-		}
-	});
-
-}
+		parkDisplay.appendChild(pTitle);
+		parkDisplay.appendChild(pStates);
+	}
+};
+apiReq.send();
