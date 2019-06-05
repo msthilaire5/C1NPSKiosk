f5d948d801ad43e0c999bfbffa41adfcbf2116f1
Megan St. Hilaire <mfsthilaire13@gmail.com>
2019-06-05 09:31:15 -0400

    Location Results Display with Links

--- searchResults.js
+++ searchResults.js
@@ -9,32 +9,41 @@ function dispLocRes() {
 
 	const locURL = "https://developer.nps.gov/api/v1/parks?api_key=" + API_KEY ;
 
-	var apiReq = new XMLHttpRequest();
-
-	apiReq.open('GET', locURL, true);
-	apiReq.onload = function() {
+	$.getJSON(locURL, function(data) {
 		// div displaying results
-		var parkDisplay = document.getElementById('locationResults');
-		while (parkDisplay.firstChild) {
-	    	parkDisplay.removeChild(parkDisplay.firstChild);
-		}
+		var parkDisplay = $("#locationResults");
+		// Remove "Loading results..." filler.
+		parkDisplay.empty();
 
-		var parkList = JSON.parse(this.response).data;
+		// List of all parks
+		var parkList = data.data;
 
+		// Populate with name header and state locat for each park.
 		for(pIndex = 0; pIndex < 50; pIndex++) {
-			park = parkList[pIndex];
+			park = parkList[pIndex]; // Select-a-park!
+			var resBox = $("<div class='resBox'></div>"); // Box it!
 
-			const pTitle = document.createElement('h3');
-			pTitle.textContent = park.fullName;
+			// URL for filling template.
+			const pCode = park.parkCode;
+			const templateUrl = "park.html?parkCode=" + pCode;
 
-			const pStates = document.createElement('p');
-			pStates.textContent = park.states;
+			const pLink = $("<a class='resLink' href=" + templateUrl + "></a>").text(park.fullName);
+			const pTitle = $("<h3></h3>").html(pLink);
+			const pStates = $("<p></p>").text( "(" + park.states + ")");
 
-			parkDisplay.appendChild(pTitle);
-			parkDisplay.appendChild(pStates);
+
+			resBox.append(pTitle);
+			resBox.append(pStates);
+			/* Apparently park data doesn't come with pics... I was lied to?
+			if (park.images) {
+				const pImgURL = park.images[0].url;
+				const pImg = $("<img class='resBoxImg' src='" + pImgURL + "'></img>");
+				resBox.append(pImg);
+			}
+			*/
+			parkDisplay.append(resBox);
 		}
-	};
-	apiReq.send();
+	});
 
 	return;
 }
