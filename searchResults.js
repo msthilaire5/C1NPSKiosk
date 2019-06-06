6c3b4c309885b64e6b618a56510c693fb752ddb3
Megan St. Hilaire <mfsthilaire13@gmail.com>
2019-06-06 12:45:45 -0400

    Result Tiles!
    Black header with white text, black para text with white background. Gets shadow on hover.

--- searchResults.js
+++ searchResults.js
@@ -20,7 +20,7 @@ function dispLocRes() {
 
 		// Populate with name header and state locat for each park.
 		for(pIndex = 0; pIndex < 52; pIndex++) {
-			park = parkList[pIndex]; // Select-a-park!
+			const park = parkList[pIndex]; // Select-a-park!
 			var resBox = $("<div class='resBox'></div>"); // Box it!
 
 			// URL for filling template.
@@ -63,7 +63,7 @@ function dispEventRes() {
 
 		// Populate with name header and state locat for each park.
 		for(eIndex = 0; eIndex < 50; eIndex++) {
-			event = eventList[eIndex]; // Select-an-event!
+			const event = eventList[eIndex]; // Select-an-event!
 			var resBox = $("<div class='resBox'></div>"); // Box it!
 
 			const eTitle = $("<h3></h3>").text(event.title);
@@ -98,9 +98,9 @@ function dispNewsRes() {
 
 		// Populate with name header and state locat for each park.
 		for(nIndex = 0; nIndex < 52; nIndex++) {
-			release = newsList[nIndex]; // Select-a-release!
+			const release = newsList[nIndex]; // Select-a-release!
 
-			const rLink = $("<a class='resLink' href=" + release.url + "></a>").text(release.title);
+			const rLink = $("<a class='resLink' style='color: black;' href=" + release.url + "></a>").text(release.title);
 			const rTitle = $("<h3></h3>").html(rLink);
 			// Extract release date
 			const rDate = new Date(Date.parse(release.releasedate.substring(0,10)));
@@ -109,6 +109,41 @@ function dispNewsRes() {
 
 			newsDisplay.append(rTitle);
 			newsDisplay.append(rPara);
+		}
+	});
+
+	return;
+}
+
+function dispEduRes() {
+
+	const newsURL = "https://developer.nps.gov/api/v1/lessonplans?api_key=" + API_KEY ;
+
+	$.getJSON(newsURL, function(data) {
+		// div displaying results
+		var lpDisplay = $("#lpResults");
+		// Remove "Loading results..." filler.
+		lpDisplay.empty();
+
+		// List of all parks
+		var lpList = data.data;
+
+		// Populate with name header and state locat for each park.
+		for(lpIndex = 0; lpIndex < 52; lpIndex++) {
+			const lPlan = lpList[lpIndex]; // Select-a-release!
+			var resBox = $("<div class='resBox'></div>"); // Box it!
+
+			const lpLink = $("<a class='resLink' href=" + lPlan.url + "></a>").text(lPlan.title);
+			const lpTitle = $("<h3></h3>").html(lpLink);
+			// Extract release date
+			const lpSub = $("<p></p>").text(lPlan.subject);
+			const lpGrade = $("<p></p>").text(lPlan.gradelevel);
+
+			resBox.append(lpTitle);
+			resBox.append(lpSub);
+			resBox.append(lpGrade);
+
+			lpDisplay.append(resBox);
 		}
 	});
 
